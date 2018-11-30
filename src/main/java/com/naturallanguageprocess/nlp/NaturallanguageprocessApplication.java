0afdf8222f9479b583c6becb2745e55e29786b5f
menty44 <menty44@gmail.com>
2018-11-30 12:19:08 +0300

    initial commit

---
+++ src/main/java/com/naturallanguageprocess/nlp/NaturallanguageprocessApplication.java
@@ -0,0 +1,12 @@
+package com.naturallanguageprocess.nlp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NaturallanguageprocessApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NaturallanguageprocessApplication.class, args);
+	}
+}
